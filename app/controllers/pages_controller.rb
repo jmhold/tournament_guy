9c365cfc5bd0b5ee43706cd8d97f843e3888fbd2
Jarrod Holder <jarrodh@theswitch.us>
2014-07-04 14:43:58 -0500

    initial setup for page structureing

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+
+	def home
+	end
+
+	def account_created
+	end
+
+	def tournament_search
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  get "pages/home"
+
+  get "pages/account_created"
+
+  get "pages/tournament_search"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
